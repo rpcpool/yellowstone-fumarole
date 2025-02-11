1190182e974a35730e50f15499d1cd21cbff7386
Louis-Vincent <louis-vincent@triton.one>
2025-02-11 13:39:14 -0500

    added drain on Ctrl+C

--- fume/fume/cli/stream_commands.py
+++ fume/fume/cli/stream_commands.py
@@ -1,6 +1,11 @@
+from dataclasses import dataclass
 import multiprocessing
+import multiprocessing.queues
+import queue
 import random
+import signal
 import string
+from typing import Optional
 import click
 from fume.grpc import (
     FumaroleClient,
@@ -14,6 +19,21 @@ def generate_random_cg_name():
     """Generate a random consumer group name."""
     random_suffix = "".join(random.choices(string.ascii_lowercase, k=6))
     return f"fume-{random_suffix}"
+
+
+@dataclass
+class StopFumaroleStream:
+    pass
+
+
+@dataclass
+class FumaroleStreamData:
+    data: any
+
+
+@dataclass
+class FumaroleStreamEnd:
+    pid: int
 
 
 @click.command()
@@ -89,43 +109,98 @@ def stream(ctx, cg_name, parallel, tx_account, account, owner, output_format):
         if owner:
             subscribe_request["accounts"]["owner"] = list(owner)
 
-    def fumarole_stream_proc(shared_q, cg_name, member_idx, endpoint, x_token):
+    def fumarole_stream_proc(
+        cnc_rx: multiprocessing.Queue,  # command-and-control queue
+        data_tx: multiprocessing.Queue,
+        cg_name: str,
+        member_idx: int,
+        endpoint: str,
+        x_token: Optional[str],
+    ):
+        signal.signal(signal.SIGINT, signal.SIG_IGN)  # Ignore Ctrl+C in subprocess
         with grpc_channel(endpoint, x_token) as channel:
             fc = FumaroleClient(channel, metadata=metadata)
-            for event in fc.subscribe(
+
+            my_pid = multiprocessing.current_process().pid
+            subscribe_iter = fc.subscribe(
                 cg_name, member_idx, mapper=output_format, **subscribe_request
-            ):
-                shared_q.put(event)
+            )
+            for event in subscribe_iter:
+                data_tx.put(FumaroleStreamData(data=event))
+                try:
+                    command = cnc_rx.get_nowait()
+                    match command:
+                        case StopFumaroleStream():
+                            break
+                except queue.Empty:
+                    pass
 
-    def stream_loop(mpq, fumarole_ps):
-        while all(p.is_alive() for p in fumarole_ps):
-            data = mpq.get()
-            click.echo(data)
+            channel.close()
+            # Flush any remaining data
+            for event in subscribe_iter:
+                data_tx.put(event)
 
-    mpq = multiprocessing.Queue()
-    fumarole_ps = []
+            data_tx.put(FumaroleStreamEnd(pid=my_pid))
+
+    data_tx = multiprocessing.Queue()
+    data_rx = data_tx
+    fumarole_ps: dict[multiprocessing.Process, int] = dict()
+    fumarole_cnc_tx_vec = []
+    fumarole_stream_id_vec = set()
     for i in range(parallel):
         j = i % len(endpoints)
         endpoint = endpoints[j]
         print(f"Spawned fumarole connection: {i}...")
-        fumarole = multiprocessing.Process(
-            target=fumarole_stream_proc, args=(mpq, cg_name, i, endpoint, x_token)
+
+        cnc_tx = multiprocessing.Queue()
+        cnc_rx = cnc_tx
+        fumarole: multiprocessing.Process = multiprocessing.Process(
+            target=fumarole_stream_proc,
+            args=(cnc_rx, data_tx, cg_name, i, endpoint, x_token),
         )
+
         fumarole.start()
         click.echo(
             f"Started fumarole connection: {i} with pid={fumarole.pid}!", err=True
         )
-        fumarole_ps.append(fumarole)
+        fumarole_ps[fumarole.pid] = fumarole
+        fumarole_cnc_tx_vec.append(cnc_tx)
+        fumarole_stream_id_vec.add(fumarole.pid)
 
-    try:
-        stream_loop(mpq, fumarole_ps)
-        click.echo("All connections closed", err=True)
-    except KeyboardInterrupt:
-        pass
-    except EOFError:
-        pass
-    finally:
-        for p in fumarole_ps:
-            p.terminate()
-        for p in fumarole_ps:
-            p.join()
+    while True:
+        try:
+            if not all(p.is_alive() for p in fumarole_ps.values()):
+                break
+            match data_rx.get():
+                case FumaroleStreamData(data):
+                    click.echo(data)
+                case FumaroleStreamEnd(pid):
+                    fumarole_stream_id_vec.remove(pid)
+                    click.echo(f"Connection {pid} ended!", err=True)
+                    break
+            click.echo(data)
+        except KeyboardInterrupt:
+            break
+        except EOFError:
+            break
+
+    for cnc_tx in fumarole_cnc_tx_vec:
+        cnc_tx.put(StopFumaroleStream())
+
+    # Drain any leftover data in case some is left in the queue.
+    while fumarole_stream_id_vec or any(p.is_alive() for p in fumarole_ps.values()):
+        try:
+            match data_rx.get(timeout=1):
+                case FumaroleStreamData(data):
+                    click.echo(data)
+                case FumaroleStreamEnd(pid):
+                    fumarole_stream_id_vec.remove(pid)
+                    fumarole_proc = fumarole_ps.pop(pid)
+                    fumarole_proc.terminate()
+                    fumarole_proc.join()
+        except queue.Empty:
+            pass
+
+    for _, fumarole_proc in fumarole_ps.items():
+        fumarole_proc.terminate()
+        fumarole_proc.join()
