2d07049c5596f74118a3f13cb82a34c8566fd572
lvboudre <louis-vincent@triton.one>
2025-08-13 09:12:19 -0400

    added OrderedSet (#23)
    * added OrderedSet
    
    * fix advisories

---
+++ python/yellowstone-fumarole-client/tests/test_ordered_set.py
@@ -0,0 +1,39 @@
+from yellowstone_fumarole_client.utils.collections import OrderedSet
+import pytest
+
+
+def test_ordered_set():
+    os = OrderedSet()
+    os.add(1)
+    os.add(2)
+    os.add(3)
+    assert list(os) == [1, 2, 3]
+    assert 2 in os
+    assert len(os) == 3
+    os.popfirst()
+    assert list(os) == [2, 3]
+    assert len(os) == 2
+
+
+def test_pop_until_empty():
+    os = OrderedSet()
+    os.add(1)
+    os.add(2)
+    os.add(3)
+    assert len(os) == 3
+    os.popfirst()
+    os.popfirst()
+    os.popfirst()
+    assert len(os) == 0
+    with pytest.raises(KeyError):
+        os.popfirst()
+
+
+def test_it_should_rearrange_order_on_reinsert():
+    os = OrderedSet()
+    os.add(1)
+    os.add(2)
+    os.add(3)
+    assert list(os) == [1, 2, 3]
+    os.add(2)  # Re-insert 2
+    assert list(os) == [1, 3, 2]  # 2 should now be at the end
---
+++ python/yellowstone-fumarole-client/yellowstone_fumarole_client/utils/collections.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+
+
+class OrderedSet:
+    """
+    A simple ordered set implementation that maintains the order of elements.
+    """
+
+    def __init__(self, iterable=None):
+        self._data = []
+        self.inner: OrderedDict = OrderedDict()
+        if iterable is not None:
+            for item in iterable:
+                self.inner[item] = None
+
+    def add(self, item):
+        self.inner[item] = None
+        # Make sure the item is at the end to maintain order
+        self.inner.move_to_end(item, last=True)
+
+    def popfirst(self):
+        try:
+            self.inner.popitem(last=False)
+        except KeyError as e:
+            raise KeyError(f"{OrderedDict.__name__} is empty") from e
+
+    def __contains__(self, item):
+        return item in self.inner
+
+    def __iter__(self):
+        return iter(self.inner.keys())
+
+    def __len__(self):
+        return len(self.inner)
+
+    def __repr__(self):
+        return f"OrderedSet({list(self._data)})"
